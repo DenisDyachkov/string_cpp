cc8bd56d84939c8ecb50ea15acc21b5a6a70433c
Denis Dyachkov <denisrafdyachkov@yandex.ru>
2018-07-02 13:49:57 +0700

    Run argument

--- example.cpp
+++ example.cpp
@@ -1,7 +1,6 @@
 #include "string/string.hpp"
 
-int main()
-{
+int main(int argc, char** argv) {
     string test;
     while (std::cin >> test) {
         std::cout << "Enter string \"" << test << "\" " << test.length() << " " << test.capacity() << std::endl;
